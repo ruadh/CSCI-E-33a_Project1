31ccbaba2aed0940736a8cc9d50f80a741adc808
Jessica Donnelly Reed <ruadh@alum.mit.edu>
2021-02-11 12:42:40 -0500

    entry markup conversion

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("entries/<str:entry>", views.entry, name="entry")
+    path("entries/<str:entry>", views.entry, name="entry"),
+    path("search_results", views.search_results, name="search_results")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render
 
 from . import util
 
+from markdown2 import Markdown
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+# Render a single wiki entry
 def entry(request, entry):
-    return render(request, "encyclopedia/entry.html", { "entryContent": util.get_entry(entry), "entryName": entry } )
+    html = Markdown()
+    txt = html.convert(util.get_entry(entry))
+    return render(request, "encyclopedia/entry.html", { "entry_content": txt, "entry_title": entry } )
+
+# Render the search results page
+def search_results(request):
+    return render(request, "encyclopedia/search-results.html", {
+        "entries": util.list_entries()
+    })
